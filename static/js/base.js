eea8657e95fb3a75b0394f6e4175d6cadd3a9539
Linus Rogge <hello@linusrogge.com>
2023-04-26 20:41:51 +0200

    (add) footer

--- static/js/base.js
+++ static/js/base.js
@@ -7,3 +7,30 @@ window.onscroll = () => {
         navigation.classList.remove('--muted')
     }
 }
+(function() {
+    var quotes = [
+        {
+            text: "You will never influence the world by trying to be like it.",
+            author: "Travis Scott"
+        },
+        {
+            text: "Everybody is a genius. But if you judge a fish by its ability to climb a tree, it will live its whole life believing that it is stupid.",
+            author: "Albert Einstein"
+        },
+        {
+            text: "What you admire in others exist within you"
+        },
+        {
+            text: "We accept the love we think we deserve",
+            author: "From: The Perks of Being a Wallflower"
+        },
+        {
+            text: "Always deliver more than expected.",
+            author: "Larry Page"
+        }
+    ]
+    var quote = quotes[Math.floor(Math.random() * quotes.length)];
+    document.querySelector('.footer blockquote p').innerHTML = quote.text;
+    document.querySelector('.footer blockquote cite').innerHTML = quote.author ? quote.author : '';
+})();
+
